eeaa75a39da579cf4785b550d701014b99b8a1ac
VLiamin <LyaminVA@yandex.ru>
2020-11-09 03:45:32 +0300

    GeneralModal added

---
+++ Tablet/Data/Models/GeneralDevelopment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tablet.Data.Models
+{
+    public class GeneralDevelopment
+    {
+        private readonly AppDBContent appDBContent;
+
+        public GeneralDevelopment(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+
+        public DateTime Date { get; set; }
+
+
+    }
+}
