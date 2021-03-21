fde8284ed7470917ed8c913db73c1189ee2b26d5
VLiamin <LyaminVA@yandex.ru>
2021-03-22 02:53:34 +0300

    added GUI

---
+++ Gui/ViewModels/ProjectListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tablet.Data.Models;
+
+namespace Tablet.ViewModels
+{
+    public class ProjectListViewModel
+    {
+        public ProjectModels ProjectModels { get; set; }
+        public Project Project { get; set; }
+    }
+}
