341c649b378ab8786b4b1f2ad21666c13e82c288
VLiamin <LyaminVA@yandex.ru>
2021-03-25 18:10:46 +0300

    fjjf

--- Gui/Pages/Index.cshtml.cs
+++ Gui/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Gui.Pages
+namespace GUI.Pages
 {
     public class IndexModel : PageModel
     {
