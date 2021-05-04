206170ba1e2baee2283aeb271478d807b76577ca
VLiamin <LyaminVA@yandex.ru>
2021-05-04 14:51:27 +0300

    added RestrictionsModel

---
+++ Tablet/Data/Models/Restrictions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tablet.Data.Models
+{
+    public class Restrictions
+    {
+        String Id { get; set; }
+        String ProjectId { get; set; }
+        String RedLine { get; set; }
+        String Finance { get; set; }
+        String License { get; set; }
+        String Architecture { get; set; }
+        String Safety { get; set; }
+        String Data { get; set; }
+        String Document { get; set; }
+        String Infrastructure { get; set; }
+    }
+}
