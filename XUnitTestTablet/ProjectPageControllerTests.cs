c6c3f2873896029904875504aa0643f6170cc274
VLiamin <LyaminVA@yandex.ru>
2021-05-20 00:29:30 +0300

    add new tests

---
+++ XUnitTestTablet/ProjectPageControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tablet.Controllers;
+using Tablet.Data;
+using Tablet.Data.Models;
+using Xunit;
+
+namespace XUnitTestTablet
+{
+    public class ProjectPageControllerTests
+    {
+        [Fact]
+        public void AddProblemTest()
+        {
+
+            AppDBContent app = new AppDBContent();
+            ProjectPageController controller = new ProjectPageController(new ProjectPageModel(app));
+
+            ViewResult result = controller.AddProblem() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void AddRisksTest()
+        {
+
+            AppDBContent app = new AppDBContent();
+            ProjectPageController controller = new ProjectPageController(new ProjectPageModel(app));
+
+            ViewResult result = controller.AddRisks() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void AddStageTest()
+        {
+
+            AppDBContent app = new AppDBContent();
+            ProjectPageController controller = new ProjectPageController(new ProjectPageModel(app));
+
+            ViewResult result = controller.AddStage() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void AddGeneralWorksTest()
+        {
+
+            AppDBContent app = new AppDBContent();
+            ProjectPageController controller = new ProjectPageController(new ProjectPageModel(app));
+
+            ViewResult result = controller.AddGeneralWorks() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void AddGeneralProblemTest()
+        {
+
+            AppDBContent app = new AppDBContent();
+            ProjectPageController controller = new ProjectPageController(new ProjectPageModel(app));
+
+            ViewResult result = controller.AddGeneralProblem() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}
