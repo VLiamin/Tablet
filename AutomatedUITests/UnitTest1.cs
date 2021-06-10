d5354dd067586b12e89389febc609843b30fdb7b
VLiamin <LyaminVA@yandex.ru>
2021-06-11 02:40:42 +0300

    more tests

--- AutomatedUITests/UnitTest1.cs
+++ AutomatedUITests/UnitTest1.cs
@@ -11,13 +11,26 @@ namespace AutomatedUITests
         {
             driver = new OpenQA.Selenium.Chrome.ChromeDriver();
             driver.Navigate().GoToUrl("https://localhost:44351/");
-
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsNotNull(driver.Title);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            By by = By.XPath("//a[text()='Добавить проект']");
+            Assert.IsNotNull(driver.FindElement(by));
         }
     }
 }
